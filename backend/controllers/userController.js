984b076040ad017e25196c81ab2fe26d83538b75
ingucell <mathiaslawson70@gmail.com>
2022-12-14 23:34:33 +0000

    error handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,12 @@
 const registerUser = (req, res) =>{
+    const {name, email , password} = req.body
+
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all field')
+    }
+
     res.send('Register Route')
-    console.log(req.body)
 }
 const loginUser = (req, res) =>{
     res.send('login Route')
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const dotenv= require('dotenv').config()
 const PORT = process.env.PORT || 9000
 const app = express()
+const {errorHandler} = require('../backend/middleware/errorMiddleware')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+
 app.use('/api/users', require('./routes/userRoutes'))
 
-
+app.use(errorHandler)
 
 app.listen(PORT, ()=>console.log(`Server is running no ${PORT}`))
