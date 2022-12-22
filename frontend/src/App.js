04e268b1adadff3f90e49e3e690497145fc88e07
ingucell <mathiaslawson70@gmail.com>
2022-12-22 15:19:33 +0000

    icons, pages setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Header from './components/Header';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
      <>
+            <Router>
+              <div className='container'>
+                <Header />
+                <Routes>
+                     <Route path="/" element={<Home />} />
+                     <Route path="/login" element={<Login />} />
+                     <Route path="/register" element={<Register />} />
+                </Routes>
+              </div>
+            </Router>
      </>
   )
 }
